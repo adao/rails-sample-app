bb366755861c37de5ffa7ea09e63d698b11b70dd
Anthony Dao <nong.shin@gmail.com>
2023-02-20 16:46:51 -0800

    Add initial page

---
+++ app/javascript/pages/index.js
@@ -0,0 +1,22 @@
+import { render } from "react-dom"
+import h from 'tools/htm-create-element';
+
+function Header() {
+  return h`
+    <header>
+      <img style=${{width: 200}} src="https://askmybook.com/static/book.2a513df7cb86.png" />
+    </header>
+  `
+}
+
+function PageIndex() {
+  return h`
+    <${Header} />
+  `
+}
+
+
+render(
+  h`<${PageIndex} />`,
+  document.getElementById("pages-index-root")
+)
